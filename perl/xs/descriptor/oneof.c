0c0f520af850a7c88829523d836eb5f60a28491c
C.J. Collier <cjac@google.com>
2026-03-29 02:52:55 +0000

    Implement OneofDef C wrappers and tests
    * perl/xs/descriptor/oneof.h: Define C wrapper API for `upb_OneofDef`
      functions, including accessors for FullName, Name, FieldCount,
      Field, ContainingType, Index, IsSynthetic, LookupName, and LookupNumber.
    * perl/xs/descriptor/oneof.c: Implement the declared wrapper functions
      to interact with `upb_OneofDef`.
    * perl/t/c/descriptor/oneof.c: Add C-level tests to verify the
      correctness of the OneofDef wrapper functions against a loaded test
      descriptor (`test_oneof` in `test.TestMessage`).
    * perl/t/c/c_test_config.json: Add the `descriptor/oneof` target with
      its necessary dependencies (including upb reflection headers) to
      the C test configuration.

--- perl/xs/descriptor/oneof.c
+++ perl/xs/descriptor/oneof.c
@@ -1,5 +1,41 @@
-#define PERL_NO_GET_CONTEXT
-#include "EXTERN.h"
-#include "perl.h"
-#include "XSUB.h"
-#include "perl/xs/descriptor/oneof.h"
+#include <sys/types.h>
+#include <setjmp.h>
+#include <stdlib.h>
+
+#include "xs/descriptor/oneof.h"
+
+const char* PerlUpb_OneofDef_FullName(pTHX_ const upb_OneofDef *o) {
+    return upb_OneofDef_FullName(o);
+}
+
+const char* PerlUpb_OneofDef_Name(pTHX_ const upb_OneofDef *o) {
+    return upb_OneofDef_Name(o);
+}
+
+int PerlUpb_OneofDef_FieldCount(pTHX_ const upb_OneofDef *o) {
+    return upb_OneofDef_FieldCount(o);
+}
+
+const upb_FieldDef* PerlUpb_OneofDef_Field(pTHX_ const upb_OneofDef *o, int i) {
+    return upb_OneofDef_Field(o, i);
+}
+
+const upb_MessageDef* PerlUpb_OneofDef_ContainingType(pTHX_ const upb_OneofDef *o) {
+    return upb_OneofDef_ContainingType(o);
+}
+
+uint32_t PerlUpb_OneofDef_Index(pTHX_ const upb_OneofDef *o) {
+    return upb_OneofDef_Index(o);
+}
+
+bool PerlUpb_OneofDef_IsSynthetic(pTHX_ const upb_OneofDef *o) {
+    return upb_OneofDef_IsSynthetic(o);
+}
+
+const upb_FieldDef* PerlUpb_OneofDef_LookupName(pTHX_ const upb_OneofDef *o, const char *name) {
+    return upb_OneofDef_LookupName(o, name);
+}
+
+const upb_FieldDef* PerlUpb_OneofDef_LookupNumber(pTHX_ const upb_OneofDef *o, uint32_t num) {
+    return upb_OneofDef_LookupNumber(o, num);
+}
--- perl/xs/descriptor/oneof.h
+++ perl/xs/descriptor/oneof.h
@@ -6,6 +6,14 @@
 #include "perl/xs/descriptor/base.h"
 #include "upb/reflection/def.h"
 
-// OneofDescriptor specific functions
+const char* PerlUpb_OneofDef_FullName(pTHX_ const upb_OneofDef *o);
+const char* PerlUpb_OneofDef_Name(pTHX_ const upb_OneofDef *o);
+int PerlUpb_OneofDef_FieldCount(pTHX_ const upb_OneofDef *o);
+const upb_FieldDef* PerlUpb_OneofDef_Field(pTHX_ const upb_OneofDef *o, int i);
+const upb_MessageDef* PerlUpb_OneofDef_ContainingType(pTHX_ const upb_OneofDef *o);
+uint32_t PerlUpb_OneofDef_Index(pTHX_ const upb_OneofDef *o);
+bool PerlUpb_OneofDef_IsSynthetic(pTHX_ const upb_OneofDef *o);
+const upb_FieldDef* PerlUpb_OneofDef_LookupName(pTHX_ const upb_OneofDef *o, const char *name);
+const upb_FieldDef* PerlUpb_OneofDef_LookupNumber(pTHX_ const upb_OneofDef *o, uint32_t num);
 
 #endif // PERL_PROTOBUF_DESCRIPTOR_ONEOF_H_
